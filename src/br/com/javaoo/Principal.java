d04a3737211427f21d63f09d6ef39d591cdc465c
davikrig-ha <daviluia2@gmail.com>
2021-08-03 23:17:53 -0300

    java orientado a objetos

---
+++ src/br/com/javaoo/Principal.java
@@ -0,0 +1,41 @@
+package br.com.javaoo;
+
+import br.com.javaoo.entities.Product;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner s = new Scanner(System.in);
+        Product p = new Product();
+
+
+        System.out.println("Enter product data");
+
+        System.out.print("name: ");
+        p.setName(s.next());
+        System.out.print("price: ");
+        p.setPrice(s.nextDouble());
+        System.out.print("quantity: ");
+        p.setQuantity(s.nextInt());
+
+
+        System.out.println("Product data: " + p.getNAME() + ", $ " + p.getPrice() + ", " +
+                p.getQuantity() + " units, total: $ " + p.totalValueInStock() );
+
+
+        System.out.println("enter the number of products to be added in stock: ");
+        p.addProduct(s.nextInt());
+
+        System.out.println("Product data: " + p.getNAME() + ", $ " + p.getPrice() + ", " +
+                p.getQuantity() + " units, total: $ " + p.totalValueInStock() );
+
+        System.out.print("enter the number of products to be removed from stock: ");
+        p.removeProduct(s.nextInt());
+
+        System.out.println("Product data: " + p.getNAME() + ", $ " + p.getPrice() + ", " +
+                p.getQuantity() + " units, total: $ " + p.totalValueInStock() );
+
+    }
+}
---
+++ src/br/com/javaoo/entities/Product.java
@@ -0,0 +1,39 @@
+package br.com.javaoo.entities;
+
+public class Product {
+
+    //declaração.
+    private String name;
+    private double price;
+    private int quantity;
+    private String color;
+
+
+    //Setando e Capturando o objeto.
+    public void setName(String nome){
+        this.name = nome;
+    }
+    public String getNAME(){
+       return name;
+    }
+
+    public void setPrice(double price){
+        this.price = price;
+    }
+    public double getPrice(){ return price; }
+
+    public void setQuantity(int quantity){ this.quantity = quantity;
+    } public int getQuantity(){ return quantity; }
+
+
+    public void setColor(String color){ this.color = color; }
+    public String getColor(){ return color; }
+
+    public double totalValueInStock(){ return   quantity * price; }
+
+    public void addProduct(int quantity){
+        if (quantity < 0){ return; } this.quantity += quantity; }
+
+    public void removeProduct(int quantity){
+        if (this.quantity < quantity){ return; } this.quantity -= quantity; }
+}
